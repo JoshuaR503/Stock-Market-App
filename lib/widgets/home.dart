d6cbeaf0e32cfe5e4773a15a1ac10338c418625d
Joshua Garcia <josshuagarciia000@gmail.com>
2020-03-14 11:25:29 -0700

    Removed hardcoded data

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -25,40 +25,36 @@ class StockMarketAppHome extends StatefulWidget {
 
 class _StockMarketAppHomeState extends State<StockMarketAppHome> {
 
-  BlocBuilder<StocksBloc, StocksState> _renderContent() {
-    return BlocBuilder<StocksBloc, StocksState>(
-      builder: (BuildContext context, StocksState state) {
-
-        if (state is StocksInitial) {
-          final symbols = 'AAPL,MSFT,V,MA,FB,JNJ,CVX'.split(',');
-
-          BlocProvider
-          .of<StocksBloc>(context)
-          .add(FetchStocks(symbols: symbols));
-        }
-        
-        if (state is StocksLoading) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        if (state is StocksLoaded) {
-          return Portfolio(stocks: state.stocks);
-        } 
-
-        return Center(child: CircularProgressIndicator());    
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0XFF1e1e1e),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
           alignment: Alignment.center,
-          child: _renderContent(),
+          child: BlocBuilder<StocksBloc, StocksState>(
+            builder: (BuildContext context, StocksState state) {
+              if (state is StocksInitial) {
+                final symbols = 'AAPL,MSFT,V,MA,FB,JNJ,CVX'.split(',');
+
+                BlocProvider
+                .of<StocksBloc>(context)
+                .add(FetchStocks(symbols: symbols));
+              }
+
+              if (state is StocksLoading) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              if (state is StocksLoaded) {
+                return Portfolio(stocks: state.stocks);
+              } 
+
+              return Center(child: CircularProgressIndicator());    
+            },
+          ),
+
+          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 40.0),
         )
       ),
     );
