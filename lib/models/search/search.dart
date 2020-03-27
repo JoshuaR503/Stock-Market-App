81d58159a1e98e60cd361445be94c46aeb831484
Joshua Garcia <josshuagarciia000@gmail.com>
2020-03-26 17:46:07 -0700

    Optimized Search Model

--- lib/models/search/search.dart
+++ lib/models/search/search.dart
@@ -1,25 +1,12 @@
-
 class StockSearch {
   final String s1Symbol;
   final String s2Name;
   final String s3Type;
-  final String s4Region;
-  final String s5MarketOpen;
-  final String s6MarketClose;
-  final String s7Timezone;
-  final String s8Currency;
-  final String s9MatchScore;
 
   StockSearch({
     this.s1Symbol,
     this.s2Name,
     this.s3Type,
-    this.s4Region,
-    this.s5MarketOpen,
-    this.s6MarketClose,
-    this.s7Timezone,
-    this.s8Currency,
-    this.s9MatchScore
   });
 
   static List<StockSearch> convertToList(List<dynamic> json) {
@@ -32,7 +19,6 @@ class StockSearch {
     });
 
     return list;
-
   }
 
   factory StockSearch.fromJson(Map<String, dynamic> json) {
@@ -40,12 +26,6 @@ class StockSearch {
       s1Symbol:json['1. symbol'],
       s2Name:json['2. name'],
       s3Type:json['3. type'],
-      s4Region:json['4. region'],
-      s5MarketOpen:json['5. marketOpen'],
-      s6MarketClose:json['6. marketClose'],
-      s7Timezone:json['7. timezone'],
-      s8Currency:json['8. currency'],
-      s9MatchScore:json['9. matchScore'],
     );
   }
 
@@ -54,12 +34,7 @@ class StockSearch {
     data['1. symbol'] = this.s1Symbol;
     data['2. name'] = this.s2Name;
     data['3. type'] = this.s3Type;
-    data['4. region'] = this.s4Region;
-    data['5. marketOpen'] = this.s5MarketOpen;
-    data['6. marketClose'] = this.s6MarketClose;
-    data['7. timezone'] = this.s7Timezone;
-    data['8. currency'] = this.s8Currency;
-    data['9. matchScore'] = this.s9MatchScore;
+
     return data;
   }
 }
--- lib/widgets/profile/screen.dart
+++ lib/widgets/profile/screen.dart
@@ -52,23 +52,26 @@ class ProfileScreen extends StatelessWidget {
       ? '${this.quote.change}  ${this.quote.changesPercentage}%'
       : '+${this.quote.change}  +${this.quote.changesPercentage}%';
 
+    final priceStyle = TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+    );
+
+    final changeStyle = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: this.color
+    );
+
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('\$${NumberFormat().format(this.quote.price)}', style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          )),
-
+          Text('\$${NumberFormat().format(this.quote.price)}', style: priceStyle),
           SizedBox(height: 4),
-          Text(text, style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: this.color
-          )),
+          Text(text, style: changeStyle),
         ],
       ),
     );
@@ -95,9 +98,9 @@ class ProfileScreen extends StatelessWidget {
         padding: EdgeInsets.only(left: 26, right: 26, top: 26),
         children: <Widget>[
           this._renderTop(),
-
           SizedBox(height: 4),
           this._renderChange(),
+          SizedBox(height: 4),
           this._renderGraph(),
           StatisticsWidget(quote: this.quote),
           CompanyProfileWidget(profile: this.profile),
