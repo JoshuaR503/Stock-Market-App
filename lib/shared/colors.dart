3bcdbe0700e156aaea63f56a372bde447d4eee95
Joshua Garcia <josshuagarciia000@gmail.com>
2020-04-01 09:35:21 -0700

    Changed app identifier for IOS

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kTileColor = const Color(0XFF2c2c2c);
+const kTileColor = const Color(0XFF222222);
 const kNegativeColor = const Color(0XFFbc4241);
 const kPositiveColor = const Color(0XFF0f9d58);
 const kLightGray = const Color(0XFFcdcdcd);
 const kLighterGray = const Color(0XFF898989);
 const kGray = const Color(0XFF757575);
-const kScaffoldBackground = const Color(0XFF1e1e1e);
+const kScaffoldBackground = const Color(0XFF181818);
