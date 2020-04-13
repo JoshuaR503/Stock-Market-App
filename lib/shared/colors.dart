66793208edaa179c01f76dd8115e71ec68e5ff3d
Joshua Garcia <josshuagarciia000@gmail.com>
2020-04-13 08:57:14 -0700

    Changed colors to a darker dark xd.

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const kTileColor = const Color(0XFF222222);
+const kTileColor = const Color(0XFF181818);
 const kNegativeColor = const Color(0XFFbc4241);
 const kPositiveColor = const Color(0XFF17a862);
 const kLightGray = const Color(0XFFcdcdcd);
 const kLighterGray = const Color(0XFF898989);
 const kGray = const Color(0XFF757575);
-const kScaffoldBackground = const Color(0XFF181818);
+const kScaffoldBackground = const Color(0XFF0e0e0e);
 
 const kTransparentWhite = const Color(0X88ffffff);
