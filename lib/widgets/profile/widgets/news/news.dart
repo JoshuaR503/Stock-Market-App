5c42ac829caac499ff29b1a4b34f978998607523
Joshua Garcia <josshuagarciia000@gmail.com>
2020-04-19 17:32:49 -0700

    Added launchUrl to the profile section.

--- lib/widgets/profile/widgets/news/news.dart
+++ lib/widgets/profile/widgets/news/news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sma/helpers/url/url.dart';
 import 'package:sma/models/profile/news/stock_news.dart';
 import 'package:sma/shared/colors.dart';
 import 'package:sma/widgets/profile/widgets/styles.dart';
@@ -29,39 +30,42 @@ class ProfileNewsScreen extends StatelessWidget {
   }
 
   Widget _buildNewsCard(StockNews singleNew) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          singleNew.source,
-          style: TextStyle( 
-            color: kLighterGray,
-            fontWeight: FontWeight.w600
+    return GestureDetector(
+      onTap: () => launchUrl(singleNew.url),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            singleNew.source,
+            style: TextStyle( 
+              color: kLighterGray,
+              fontWeight: FontWeight.w600
+            ),
+          ),  
+          SizedBox(height: 8),  
+          Text(
+            singleNew.headline, style: TextStyle(
+              height: 1.5,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ), 
+            maxLines: 2, 
+            overflow: TextOverflow.ellipsis,
           ),
-        ),  
-        SizedBox(height: 8),  
-        Text(
-          singleNew.headline, style: TextStyle(
-            height: 1.5,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ), 
-          maxLines: 2, 
-          overflow: TextOverflow.ellipsis,
-        ),
-        SizedBox(height: 8),
-        Text(
-          singleNew.summary, style: TextStyle(
-            color:  kLighterGray,
-            height: 1.8,
-            fontWeight: FontWeight.w600,
+          SizedBox(height: 8),
+          Text(
+            singleNew.summary, style: TextStyle(
+              color:  kLighterGray,
+              height: 1.8,
+              fontWeight: FontWeight.w600,
+            ),
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
           ),
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-        ),
-        SizedBox(height: 8),
-        Divider(),
-      ],
+          SizedBox(height: 8),
+          Divider(),
+        ],
+      ),
     );
   }
 }
