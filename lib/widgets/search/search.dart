73de2a5904e0abe784c0f32c2c7551a8c9e220bb
Joshua Garcia <josshuagarciia000@gmail.com>
2020-04-20 16:06:09 -0700

    Small improvement in the search section.

--- lib/widgets/search/search.dart
+++ lib/widgets/search/search.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:sma/shared/styles.dart';
+
 import 'package:sma/widgets/search/search/search_content.dart';
 import 'package:sma/widgets/search/search_box/seach_box.dart';
 
-import 'package:sma/widgets/widgets/base_screen.dart';
+import 'package:sma/widgets/widgets/base_list.dart';
 import 'package:sma/widgets/widgets/standard/header.dart';
 
 class SearchSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BaseScreen(
-      onRefresh: () async {},
+    return BaseList(
       children: [
         StandardHeader(
           title: 'Search',
